e68285665b13a3670c0f7d17d7787036e75f83aa
DX4GREY <dxablack@gmail.com>
2025-05-13 08:28:21 +0000

    add timestamped logging with tprintf and tperror for consistent output
    formatting

--- set_charge_current.c
+++ set_charge_current.c
@@ -4,11 +4,13 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <stdarg.h>
 
 #define SYSFS_PATH "/sys/class/power_supply/battery/constant_charge_current_max"
 #define INSTALL_PATH_LINUX "/usr/local/bin/setcurrent"
 #define INSTALL_PATH_TERMUX "/data/data/com.termux/files/usr/bin/setcurrent"
 
+// Timestamp helper
 void print_timestamp() {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
@@ -17,9 +19,24 @@ void print_timestamp() {
         tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
 }
 
+// printf with timestamp
+void tprintf(const char *fmt, ...) {
+    print_timestamp();
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+}
+
+// perror with timestamp
+void tperror(const char *msg) {
+    print_timestamp();
+    perror(msg);
+}
+
 void print_help(const char *prog) {
-    printf("Usage: %s <current_mA>\n", prog);
-    printf("Example: %s 2000    # Set 2000 mA (2A)\n", prog);
+    tprintf("Usage: %s <current_mA>\n", prog);
+    tprintf("Example: %s 2000    # Set 2000 mA (2A)\n", prog);
     printf("\nOptions:\n");
     printf("  --help, -h        Show this help message\n");
     printf("  --uninstall       Remove this tool from system path\n");
@@ -48,53 +65,53 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[1], "--uninstall") == 0) {
         const char *path = get_install_path();
         if (remove(path) == 0) {
-            print_timestamp(); printf("Uninstalled successfully from %s\n", path);
+            tprintf("Uninstalled successfully from %s\n", path);
             return 0;
         } else {
-            print_timestamp(); perror("Failed to uninstall");
+            tperror("Failed to uninstall");
             return 1;
         }
     }
 
     int mA = atoi(argv[1]);
     if (mA <= 0) {
-        print_timestamp(); fprintf(stderr, "Invalid input. Please enter a positive current value in mA.\n");
+        tprintf("Invalid input. Please enter a positive current value in mA.\n");
         return 1;
     }
 
     int uA = mA * 1000;
 
     // Save original permission
-    print_timestamp(); printf("Trying to stat the sysfs file...\n");
+    tprintf("Trying to stat the sysfs file...\n");
     struct stat st;
     if (stat(SYSFS_PATH, &st) != 0) {
-        print_timestamp(); perror("Failed to stat the sysfs file");
+        tperror("Failed to stat the sysfs file");
         return 1;
     }
-    print_timestamp(); printf("Success stat the sysfs file\n");
-    
-    mode_t original_mode = st.st_mode & 0777;
-	
-    // Change permission to allow writing
-    print_timestamp(); printf("Changing sysfs file permission...\n");
-    if (chmod(SYSFS_PATH, 0666) != 0) {
-        print_timestamp(); perror("Failed to change permission");
-        return 1;
-    }
-    print_timestamp(); printf("Success change permission\n");
+    tprintf("Success stat the sysfs file\n");
 
-    // Open and write to the sysfs file
-    print_timestamp(); printf("Reading sysfs file...\n");
+    mode_t original_mode = st.st_mode & 0777;
+
+    // Change permission
+    tprintf("Changing sysfs file permission...\n");
+    if (chmod(SYSFS_PATH, 0666) != 0) {
+        tperror("Failed to change permission");
+        return 1;
+    }
+    tprintf("Success change permission\n");
+
+    // Write to sysfs
+    tprintf("Reading sysfs file...\n");
     FILE *fp = fopen(SYSFS_PATH, "w");
     if (!fp) {
-        print_timestamp(); perror("Failed to open file");
+        tperror("Failed to open file");
         chmod(SYSFS_PATH, original_mode);
         return 1;
     }
 
-	print_timestamp(); printf("Writing current into sysfs file...\n");
+    tprintf("Writing current into sysfs file...\n");
     if (fprintf(fp, "%d", uA) < 0) {
-        print_timestamp(); perror("Failed to write current");
+        tperror("Failed to write current");
         fclose(fp);
         chmod(SYSFS_PATH, original_mode);
         return 1;
@@ -103,6 +120,6 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     chmod(SYSFS_PATH, original_mode);
 
-    print_timestamp(); printf("Charge current set to %d mA (%d uA)\n", mA, uA);
+    tprintf("Charge current set to %d mA (%d uA)\n", mA, uA);
     return 0;
 }
